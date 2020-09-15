3ef6b378355e022f5b8c7e65c20ed9f8d0769c43
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2020-09-15 20:29:32 +0200

    Cleanup app middleware

--- frontend/src/redux/middleware/app.js
+++ frontend/src/redux/middleware/app.js
@@ -34,49 +34,7 @@ export const appInitFlow = ({ dispatch, getState }) => (next) => (action) => {
       dispatch(saveAuthToken(token));
       dispatch(getUser());
     }
-
-    // // If the user is already logged in
-    // let state = getState();
-    // const isLoggedIn = state.auth.isLoggedIn;
-    // if (isLoggedIn) {
-    //   if (moment().isAfter(moment(state.auth.token.expires_at))) {
-    //     dispatch(refreshToken());
-    //   } else {
-    //     dispatch(initSession());
-    //   }
-    // }
   }
 };
 
-export const sessionInitFlow = ({ dispatch, getState }) => (next) => (
-  action
-) => {
-  next(action);
-
-  if (action.type === INIT_SESSION) {
-    // let state = getState();
-    // if (!state.auth.isLoggedIn) {
-    //   console.log('ERROR : User not yet logged in');
-    // }
-    // // Set inital working days if none are set.
-    // if (!Object.keys(state.whours.days).length) {
-    //   for (let i = 0; i <= 4; i++) {
-    //     dispatch(addAM(i));
-    //     dispatch(addPM(i));
-    //   }
-    // }
-    // // If the current month is not yet populated, we need to populate it
-    // const { month } = state;
-    // if (!month.length) {
-    //   dispatch(populateMonth());
-    // }
-    // // Now that the month is populatedm we now want to load the time
-    // // entries for the current month.
-    // // That way they are laready loaded when the caledar is displayed.
-    // // Next time the entries are loaded is when the current month is
-    // // changed ==> month middlware with NEXT_MONTH & PREV_MONTH actions
-    // dispatch(fetchEntries());
-  }
-};
-
-export const appMiddleware = [appInitFlow, sessionInitFlow];
+export const appMiddleware = [appInitFlow];

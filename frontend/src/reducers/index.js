bd32783e0d95e38a777f0464dd32b251e11e857d
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2020-09-15 16:12:22 +0200

    Delete unused files

--- frontend/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import AuthListReducer from './AuthListReducer';
-
-const CombinedReducer = combineReducers({
-  AuthList: AuthListReducer,
-});
-
-export default CombinedReducer;

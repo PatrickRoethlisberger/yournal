6ab88465a9e83509eb9cad18b39d648747812833
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2020-09-16 20:25:17 +0200

    Add propper handling of loading state

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -8,6 +8,7 @@ import {
 } from '@material-ui/core';
 import { Pagination } from '@material-ui/lab';
 import moment from 'moment';
+import 'moment/locale/de';
 import React from 'react';
 import _ from 'lodash';
 import { useDispatch, useSelector } from 'react-redux';
@@ -16,6 +17,8 @@ import Calendar from 'react-github-contribution-calendar';
 import {
   getPostDates,
   getPosts,
+  GET_POSTS,
+  GET_POSTSDATES,
   setPage as _setPage,
 } from '../../redux/actions/post';
 import { showNotification } from '../../redux/actions/ui';
@@ -26,6 +29,12 @@ const Home = () => {
   const dispatch = useDispatch();
 
   const postsState = useSelector((state) => state.posts);
+  const postsLoading = useSelector(
+    (state) => state.ui.pendingFeatures[GET_POSTS]
+  );
+  const statsLoading = useSelector(
+    (state) => state.ui.pendingFeatures[GET_POSTSDATES]
+  );
 
   const [page, setPage] = React.useState(1);
   const handlePageChange = (event, value) => {
@@ -51,53 +60,98 @@ const Home = () => {
         alignItems="flex-start"
         spacing={3}
       >
-        <Grid item xs={12} sm={10}>
+        <Grid item xs={12} sm={8}>
           {/* Display loading when count is null */}
-          {!postsState.count ? (
+          {postsLoading ? (
             <CircularProgress />
-          ) : (
+          ) : postsState.count ? (
             <Grid container direction="column" spacing={3}>
               {postsState.items.map((el, i) => {
+                console.log(moment(new Date(el.pubDate)).fromNow());
                 return (
                   <Grid item key={i}>
                     <PostItem
                       title={el.title}
                       category={el.category}
                       coverImage={el.coverImage}
-                      body={el.body}
+                      // body={el.body}
                       slug={el.slug}
-                      pubDate={moment(new Date(el.createdAt)).fromNow()}
+                      pubDate={moment(new Date(el.pubDate)).fromNow()}
                     />
                   </Grid>
                 );
               })}
-              <Pagination
-                count={Math.ceil(postsState.count / postsState.pageSize)}
-                page={page}
-                onChange={handlePageChange}
-              />{' '}
+              {/* Only show pagination if there are more than one page */}
+              {Math.ceil(postsState.count / postsState.pageSize) > 1 ? (
+                <Pagination
+                  count={Math.ceil(postsState.count / postsState.pageSize)}
+                  page={page}
+                  onChange={handlePageChange}
+                />
+              ) : (
+                ' '
+              )}
+            </Grid>
+          ) : (
+            <Grid container direction="column" spacing={3}>
+              <Grid item>
+                <PostItem
+                  title={'Keine Beiträge gefunden'}
+                  category={null}
+                  coverImage={null}
+                  body={
+                    'Es wurden leider keine Beiträge gefunden 😢 Erstelle einen neuen!'
+                  }
+                  slug={'editor'}
+                />
+              </Grid>
             </Grid>
           )}
         </Grid>
-        <Grid item xs={12} sm={2}>
+        <Grid item xs={12} sm={4}>
           <Grid container direction="column" spacing={3}>
-            <Grid item>
-              <Card className={classes.card}>
-                <CardContent className={classes.activityContent}>
-                  <Typography component="h2" variant="h4">
-                    Activity
-                  </Typography>
-                  {!_.isEmpty(postsState.dateCount) ? (
-                    <Calendar
-                      values={postsState.dateCount}
-                      until={moment().format('YYYY-MM-DD')}
-                    />
-                  ) : (
-                    <CircularProgress />
-                  )}
-                </CardContent>
-              </Card>
-            </Grid>
+            {!_.isEmpty(postsState.dateCount) && !statsLoading ? (
+              <Grid item>
+                <Card className={classes.card}>
+                  <CardContent className={classes.activityContent}>
+                    <Typography component="h2" variant="h4">
+                      Activity
+                    </Typography>
+                    {statsLoading ? (
+                      <CircularProgress />
+                    ) : (
+                      <Calendar
+                        values={postsState.dateCount}
+                        until={moment().format('YYYY-MM-DD')}
+                      />
+                    )}
+                  </CardContent>
+                </Card>
+              </Grid>
+            ) : (
+              <Grid item />
+            )}
+            {!_.isEmpty(postsState.dateCount) && !statsLoading ? (
+              <Grid item>
+                <Card className={classes.card}>
+                  <CardContent className={classes.activityContent}>
+                    <Typography component="h2" variant="h4">
+                      Activity
+                    </Typography>
+                    {statsLoading ? (
+                      <CircularProgress />
+                    ) : (
+                      <Calendar
+                        values={postsState.dateCount}
+                        until={moment().format('YYYY-MM-DD')}
+                      />
+                    )}
+                  </CardContent>
+                </Card>
+              </Grid>
+            ) : (
+              ' '
+            )}
           </Grid>
         </Grid>
       </Grid>

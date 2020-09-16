71a6b7640ef196f1f7689e899fdf34d8aae22066
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2020-09-16 20:26:21 +0200

    Add pagination feature

--- frontend/src/redux/middleware/post.js
+++ frontend/src/redux/middleware/post.js
@@ -1,24 +1,22 @@
 import { apiRequest, API_ERROR, API_SUCCESS } from '../actions/api';
-import { GET_POSTS, GET_POSTSDATES } from '../actions/post';
+import { getPosts, GET_POSTS, GET_POSTSDATES, SET_PAGE } from '../actions/post';
 import { hideSpinner, showNotification, showSpinner } from '../actions/ui';
 import conf from '../../conf';
+import { getUser } from '../actions/auth';
 
 export const getPostsFlow = ({ dispatch, getState }) => (next) => (action) => {
   next(action);
 
   const state = getState();
 
-  let requestUrl;
   switch (action.type) {
     case GET_POSTS:
       const page = state.posts.currentPage;
       const limit = state.posts.pageSize;
-      const requestUrl = `${conf.apiRoot}/posts`;
 
-      // URL wenn Marc Pagesize gefixt hat
-      // const requestUrl = `${
-      //   conf.apiRoot
-      // }/posts?limit=${limit}&offset=${(page - 1) * limit}`;
+      const requestUrl = `${conf.apiRoot}/posts?limit=${limit}&offset=${
+        (page - 1) * limit
+      }`;
 
       dispatch(
         apiRequest({
@@ -36,23 +34,16 @@ export const getPostsFlow = ({ dispatch, getState }) => (next) => (action) => {
       break;
 
     case `${GET_POSTS} ${API_ERROR}`:
-      dispatch(
-        showNotification(
-          'error',
-          'Leider konnten keine Posts gefunden werden - versuchen Sie es später erneut ⏳'
-        )
-      );
+      // Most probably failed due to invalid session
+      dispatch(showNotification(getUser()));
       dispatch(hideSpinner({ feature: GET_POSTS }));
       break;
   }
 };
 
-export const getPostsDatesFlow = ({ dispatch, getState }) => (next) => (
-  action
-) => {
+export const getPostsDatesFlow = ({ dispatch }) => (next) => (action) => {
   next(action);
 
-  let requestUrl;
   switch (action.type) {
     case GET_POSTSDATES:
       const requestUrl = `${conf.apiRoot}/posts`;
@@ -73,15 +64,21 @@ export const getPostsDatesFlow = ({ dispatch, getState }) => (next) => (
       break;
 
     case `${GET_POSTSDATES} ${API_ERROR}`:
-      dispatch(
-        showNotification(
-          'error',
-          'Leider konnten keine Posts gefunden werden - versuchen Sie es später erneut ⏳'
-        )
-      );
+      // Most probably failed due to invalid session
+      dispatch(showNotification(getUser()));
       dispatch(hideSpinner({ feature: GET_POSTSDATES }));
       break;
   }
 };
 
-export const postsMiddleware = [getPostsFlow, getPostsDatesFlow];
+export const setPageFlow = ({ dispatch }) => (next) => (action) => {
+  next(action);
+
+  switch (action.type) {
+    case SET_PAGE:
+      dispatch(getPosts());
+      break;
+  }
+};
+
+export const postsMiddleware = [getPostsFlow, getPostsDatesFlow, setPageFlow];
